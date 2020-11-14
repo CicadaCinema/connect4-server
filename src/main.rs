bc25674bf0f006dcd23ff68640f9d9ca7ae2e4ae
ColonisationCaptain <52425971+ColonisationCaptain@users.noreply.github.com>
2020-11-14 21:42:56 +0000

    clean up messy code some quality-of life improvements too... now, player 1 is the client which connected first

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::time::Duration;
 
 fn handle_two_clients(mut stream1: TcpStream, mut stream2: TcpStream) {
-    let mut data = [0 as u8; 1]; // using 1 byte buffer
+    // using 1 byte buffer
+    let mut data = [0 as u8; 1];
 
+    // set timeouts
+    stream1.set_read_timeout(Some(Duration::new(60, 0)));
+    stream2.set_read_timeout(Some(Duration::new(60, 0)));
+    stream1.set_write_timeout(Some(Duration::new(20, 0)));
+    stream2.set_write_timeout(Some(Duration::new(20, 0)));
+
+    // let the clients know which one is 'player 1'
     stream1.write(b"1").unwrap();
     stream2.write(b"2").unwrap();
 
+    // TODO: reduce number of lined by removing repeated code? this isn't a very high priority...
     loop {
         match stream1.read(&mut data) {
             Ok(size) => {
@@ -15,8 +25,10 @@ fn handle_two_clients(mut stream1: TcpStream, mut stream2: TcpStream) {
                 data = [0 as u8; 1];
             },
             Err(_) => {
-                println!("An error occurred, terminating connection with {}", stream1.peer_addr().unwrap());
+                //println!("An error occurred or the stream timed out, terminating connection with {}", stream1.peer_addr().unwrap());
                 stream1.shutdown(Shutdown::Both).unwrap();
+                stream2.shutdown(Shutdown::Both).unwrap();
+                break;
             }
         }
         match stream2.read(&mut data) {
@@ -25,44 +37,40 @@ fn handle_two_clients(mut stream1: TcpStream, mut stream2: TcpStream) {
                 data = [0 as u8; 1];
             },
             Err(_) => {
-                println!("An error occurred, terminating connection with {}", stream1.peer_addr().unwrap());
+                //println!("An error occurred or the stream timed out, terminating connection with {}", stream2.peer_addr().unwrap());
+                stream1.shutdown(Shutdown::Both).unwrap();
                 stream2.shutdown(Shutdown::Both).unwrap();
+                break;
             }
         }
     }
 }
 
 fn main() {
-    let mut stream_vector: Vec<TcpStream> = vec![];
-
+    let mut streams: Vec<TcpStream> = vec![];
     let listener = TcpListener::bind("0.0.0.0:32032").unwrap();
-    // accept connections and process them, spawning a new thread for each one
-    println!("Server listening on port 32032");
-
+    //println!("Server listening on port 32032");
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 //println!("New connection: {}", stream.peer_addr().unwrap());
-                println!("got another stream!");
-                stream_vector.push(stream);
-                /* thread::spawn(move|| {
-                    // connection succeeded
-                    handle_client(stream)
-                }); */
+                streams.push(stream);
             }
             Err(e) => {
+                // connection failed
                 println!("Error: {}", e);
-                /* connection failed */
             }
         }
 
-        if stream_vector.len() == 2 {
-            println!("got two streams!");
-            // replace unwrap with something else
-            let stream1 = stream_vector.pop().unwrap();
-            let stream2 = stream_vector.pop().unwrap();
-            handle_two_clients(stream1, stream2);
+        if streams.len() == 2 {
+            // remove the two latest streams from the vector and handle them in a new thread
+            // unwrapping isn't a problem here because the length of the vector is exactly 2
+            let stream2 = streams.pop().unwrap();
+            let stream1 = streams.pop().unwrap();
+            thread::spawn(|| {
+                handle_two_clients(stream1, stream2);
+            });
         }
     }
     // close the socket server
