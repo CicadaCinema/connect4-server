52862a364c335b9e0034e941cb992caa7de7abcb
ColonisationCaptain <52425971+ColonisationCaptain@users.noreply.github.com>
2020-12-04 18:58:44 +0000

    Add win condition

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,84 @@ use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
 use std::time::Duration;
 
+// sees if the instruction (which should already be applied to state) results in a game win for that player
+fn check_win(state: &[[u8; 7]; 6], instruction: &[u8; 3]) -> bool {
+    let player_id = &instruction[2];
+    let mut count_4;
+
+    // check horizontally
+    count_4 = 0;
+    for i in 0..7 {
+        let cell_value = &state[instruction[0] as usize][i];
+        if cell_value == player_id {
+            count_4 += 1;
+        } else {
+            count_4 = 0;
+        }
+
+        if count_4 == 4{
+            return true;
+        }
+    }
+
+    // check vertically
+    count_4 = 0;
+    for i in 0..6 {
+        let cell_value = &state[i][instruction[1] as usize];
+        if cell_value == player_id {
+            count_4 += 1;
+        } else {
+            count_4 = 0;
+        }
+
+        if count_4 == 4{
+            return true;
+        }
+    }
+
+    // check diagonally changing coords by (1, 1)
+    count_4 = 0;
+    for i in -3..4 {
+        if (0..6).contains(&(instruction[0] as i32 + i)) && (0..7).contains(&(instruction[1] as i32 + i)) {
+            let coord_0 = (instruction[0] as i32 + i) as usize;
+            let coord_1 = (instruction[1] as i32 + i) as usize;
+
+            let cell_value = &state[coord_0][coord_1];
+            if cell_value == player_id {
+                count_4 += 1;
+            } else {
+                count_4 = 0;
+            }
+
+            if count_4 == 4{
+                return true;
+            }
+        }
+    }
+
+    // check diagonally changing coords by (1, -1)
+    count_4 = 0;
+    for i in -3..4 {
+        if (0..6).contains(&(instruction[0] as i32 + i)) && (0..7).contains(&(instruction[1] as i32 - i)) {
+            let coord_0 = (instruction[0] as i32 + i) as usize;
+            let coord_1 = (instruction[1] as i32 - i) as usize;
+
+            let cell_value = &state[coord_0][coord_1];
+            if cell_value == player_id {
+                count_4 += 1;
+            } else {
+                count_4 = 0;
+            }
+
+            if count_4 == 4{
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
 fn handle_two_clients(mut stream1: TcpStream, mut stream2: TcpStream) {
     let mut streams = [stream1, stream2];
 
@@ -47,27 +125,37 @@ fn handle_two_clients(mut stream1: TcpStream, mut stream2: TcpStream) {
                     data_to_send[0] = free_cells_in_column[received_data[0] as usize];
                     data_to_send[1] = received_data[0];
 
+                    // write the instruction to the state
+                    state[data_to_send[0] as usize][data_to_send[1] as usize] = data_to_send[2];
+
+                    // if this was a winning move, indicate this in the instruction
+                    if check_win(&state, &data_to_send) {
+                        data_to_send[2] += 3;
+
+                        // send the final instruction
+                        streams[0].write(&data_to_send).unwrap();
+                        streams[1].write(&data_to_send).unwrap();
+
+                        // end the connection to the clients
+                        break;
+                    }
+
                     // pass the instructions to both clients
                     streams[0].write(&data_to_send).unwrap();
                     streams[1].write(&data_to_send).unwrap();
-
-                    // write the same instruction to the state
-                    state[data_to_send[0] as usize][data_to_send[1] as usize] = data_to_send[2];
-
-                    // TODO: implement win condition HERE
 
                     // clear received data buffer
                     received_data = [0 as u8; 1];
                 },
                 Err(_) => {
                     //println!("An error occurred or the stream timed out, terminating connection with {}", stream1.peer_addr().unwrap());
-                    streams[0].shutdown(Shutdown::Both).unwrap();
-                    streams[1].shutdown(Shutdown::Both).unwrap();
                     break;
                 }
             }
         }
     }
+    streams[0].shutdown(Shutdown::Both).unwrap();
+    streams[1].shutdown(Shutdown::Both).unwrap();
 }
 
 fn main() {
